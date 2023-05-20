bf51d2a4a9925b64c6d48f30350af53c7a5d0a37
AdamAngg <adam.nglart@gmail.com>
2023-05-20 12:33:00 +0200

    First commit from vs code

--- src/app.js
+++ src/app.js
@@ -9,14 +9,7 @@ import {
 import { stopMusic } from "./events.js";
 import * as d from "./dragEfect.js";
 
-//Blocks Hard code by class
-//Dividing blocks by looping and slicing them to array
-// const blocks11 = [];
-// const block1hh = Array.from(document.querySelectorAll(".block"));
-// for (let i = 0; i < 4; i++) {
-//   blocks11.push(block1hh.slice(i * 15, i * 15 + 15));
-// }
-// console.log(blocks11);
+
 
 //Init
 a.btnClock.addEventListener("click", onBtnClock);
